ee3a371f8b602463c206e91e7b9547f8b8a3ef17
Felipe-hen30 <henao-30@outlook.com>
2025-03-19 20:36:47 +0100

    guardando leon y coche

---
+++ src/main/java/com/afbh/demo/clases/service/Coche.java
@@ -0,0 +1,8 @@
+package com.afbh.demo.clases.service;
+
+public class Coche {
+
+	private String marca = "Ford";
+	private int modelo = 2018;
+	private double motor = 1.6;
+}
---
+++ src/main/java/com/afbh/demo/clases/service/Leon.java
@@ -0,0 +1,8 @@
+package com.afbh.demo.clases.service;
+
+public class Leon {
+
+	private String raza = "cazaraptor";
+	private int esperanzaVida = 9;
+	private double cazaSemana = 7,5;
+}
