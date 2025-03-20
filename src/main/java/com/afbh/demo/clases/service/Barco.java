c9997fe80deef6b1c795299d626fe088c601fd1a
Felipe-hen30 <henao-30@outlook.com>
2025-03-20 07:30:17 +0100

    guardando barco y bicicleta

---
+++ src/main/java/com/afbh/demo/clases/service/Barco.java
@@ -0,0 +1,8 @@
+package com.afbh.demo.clases.service;
+
+public class Barco {
+
+	private String tipo = "yate";
+	private int numeroHabitaciones = 4;
+	private double motor = 2.2;
+}
---
+++ src/main/java/com/afbh/demo/clases/service/Bicicleta.java
@@ -0,0 +1,8 @@
+package com.afbh.demo.clases.service;
+
+public class Bicicleta {
+	private String tipo = "pista";
+	private int ruedas = 2;
+	private double numeroRin = 27.5;
+
+}
